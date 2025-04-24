ef8bdd63bf30cb7a535ba5b726df3cb83fc265cf
Sebastian Pineda <sebastian14027@gmail.com>
2025-04-24 10:29:12 -0500

    Update sistema_votacion.py

--- sistema_votacion.py
+++ sistema_votacion.py
@@ -1,15 +1,20 @@
+from time import sleep
+
+print("-------SISTEMA DE VOTACION-------")
 
 candidatos = []
+numero_candidato = 1 
 
-def ingresar_candidatos():  
-    num = 1 
-    while True:        
-        ingresar_candidato = input("Ingrese el candidato: ")
-        candidatos.append({'id':num, 'nombre': ingresar_candidato.capitalize(), 'votos':0})
+while True:        
+    ingresar_candidato = input("Ingrese el candidato: ")
+
+    if ingresar_candidato.isalpha():
+        candidatos.append({'id':numero_candidato, 'nombre': ingresar_candidato.capitalize(), 'votos':0})
+
         agregar_candidato = str(input("Desea agregar más candidatos? (si/no): "))
-        
+
         if agregar_candidato.lower() == "si":
-            num += 1
+            numero_candidato += 1
             continue
         
         elif agregar_candidato.lower() == "no":
@@ -18,50 +23,53 @@ def ingresar_candidatos():
         else:
             print("Dato inválido. Intente de nuevo.")
             continue
-                
-    print("")
-    return candidatos
+    else:
+        print("Dato invalido. Intente de nuevo")
+        continue
 
-ingresar_candidatos()
+print("")
 
 while True:
     print("\n___MENU DE VOTACION___")
-    print("\nCandidatos")
+    print("\n(Si no hay mas votos por realizar, Escriba '0')")
+    print("\nCandidatos a personeria")
     for _,candidato in enumerate(candidatos):
         print(f"{_+1}.{candidato['nombre']}")
 
     voto = int(input("Por que candidato desea votar: "))
     
-    if 0 < voto <= len(candidatos):      
-        for index in candidatos:           
-            if voto == index['id']:
-                index['votos'] += 1
+    if voto == 0:
+        break
 
-        continuar_votos = str(input("Desea seguir votando? (si/no): "))
-        
-        if continuar_votos.lower() == "si":
-            continue
-
-        elif continuar_votos.lower() == "no":
-            print("\nVotación finalizada")
-            print("\nCandidatos")
-            for _,candidato in enumerate(candidatos):
-                print(f"{_+1}.{candidato['nombre']} con {candidato['votos']} votos")
-            break
-        
-        else:
-            print("Dato inválido. Intente de nuevo.")
-            continue
-        
     else:
-        print("Fuera de rango. Intente de nuevo.")
-        continue 
-
-def validate_votes(candidates):
-    for candidate in candidates:
-        for candidate_2 in candidates:
-            if candidate['votos'] > candidate_2['votos']:
-                print(f'ganó {candidate}')
+        if 0 < voto <= len(candidatos):      
+            for index in candidatos:           
+                if voto == index['id']:
+                    index['votos'] += 1
+            print("Votación exitosa!!!")
+            sleep(1)
             
-validate_votes(candidatos)           
-        
+        else:
+            print("Fuera de rango. Intente de nuevo.")
+            continue 
+
+print("\n-----Votación finalizada-----")
+print("\nCandidatos")
+for _,candidato in enumerate(candidatos,start=1):
+    print(f"{_}.{candidato['nombre']} con {candidato['votos']} votos")
+
+print("\nEn revisión...")
+sleep(1.5)
+
+
+max_votos = max(candidato['votos'] for candidato in candidatos)
+    
+ganadores = [candidato for candidato in candidatos if candidato['votos'] == max_votos]
+    
+if len(ganadores) == 1:
+    print(f'--¡Ganó {ganadores[0]["nombre"]} con {ganadores[0]["votos"]} votos!--')
+
+else:
+    print('¡Empate entre los siguientes candidatos:')
+    for ganador in ganadores:
+        print(f'{ganador["nombre"]} con un total de {ganador["votos"]} votos')
